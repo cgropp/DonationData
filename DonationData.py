4e1c6b07fd3d6c1b45cbf85716af6a3b4ae14960
cgropp <cgropp@ucsd.edu>
2023-07-18 17:38:10 -0700

    Export to csv working

--- DonationData.py
+++ DonationData.py
@@ -7,7 +7,7 @@ URL = "https://www.opensecrets.org/donor-lookup/results?cand=&cycle=2022&employ=
 site = requests.get(URL)
 data = site.text
 soup = BeautifulSoup(data, 'html.parser')
-
+page = 1
 
 
 #List of tables
@@ -17,8 +17,6 @@ tables = soup.find_all('table')
 table = soup.find('table', class_='u-mt2')
 
 # Defining of the dataframe
-
-
 header = []
 rows = []
 
@@ -30,9 +28,44 @@ for i, row in enumerate(table.find_all('tr')):
         rows.append([el.text.strip() for el in row.find_all('td')])
 
 
-
 df = pd.DataFrame(rows, columns=['Category', 'Contributor', 'Employer', 'Occupation', 'Date', 'Amount', 'Recipient', 'Recipient Jurisdiction'])
 
+#print(df_short)
+
+#Check for more pages
+nextFound = (soup.find('div', class_='paginate_button next') != None)
+
+#print(nextFound)
+
+while(nextFound):
+    page += 1
+    URL = "https://www.opensecrets.org/donor-lookup/results?cand=&cycle=2022&employ=&jurisdiction=&name=&occupation=&page=" + str(page) + "&state=&type=&zip=90623"
+    site = requests.get(URL)
+    data = site.text
+    soup = BeautifulSoup(data, 'html.parser')
+    #Extract table
+    table = soup.find('table', class_='u-mt2')
+
+    # Defining of the dataframe
+    header = []
+    rows = []
+
+    # Collecting Data
+    for i, row in enumerate(table.find_all('tr')):
+        if i == 0:
+            header = [el.text.strip() for el in row.find_all('th')]
+        else:
+            rows.append([el.text.strip() for el in row.find_all('td')])
+
+    for row in rows:
+        if(len(row) > 1):
+            df.loc[len(df)] = row
+
+    nextFound = (soup.find('div', class_='paginate_button next') != None)
+
+
+#Fix contributor names
+df['Contributor'] = df['Contributor'].str.split('\\n').str[0]
 
 #Need to add zip, phone number, address
 df_short = df[['Contributor', 'Occupation', 'Date', 'Amount', 'Recipient']]
@@ -41,5 +74,4 @@ df_short = df[['Contributor', 'Occupation', 'Date', 'Amount', 'Recipient']]
 df_short = df_short[df_short['Recipient'].str.contains("\(D\)|Democrat", na=False)]
 
 print(df_short)
-
-
+df_short.to_csv("E:\\My Documents\..CS Projects\DonationData\\testCSV.csv",index=False)
